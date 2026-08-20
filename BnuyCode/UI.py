e6814f718d88498014c4e1a4af7fd0c3c9e42de2
whenth01 <dodifoxy231@gmail.com>
2026-08-20 11:14:17 +0300

    QwQ

--- BnuyCode/UI.py
+++ BnuyCode/UI.py
@@ -167,9 +167,12 @@ To continue, please fill these fields
                     return super().keypress(size, key)
                 else: 
                     if len(text_box.get_edit_text()) > 1:
-                        interface.messages.append({interface.main_obj.data["receiver"],
-                                                  username,
-                                                  text_box.get_edit_text(),})
+                        message_dict = {
+                                "receiver": interface.main_obj.data["receiver"],
+                                "sender": username,
+                                "content": text_box.get_edit_text(),
+                                }
+                        interface.messages.append(message_dict)
                         interface.message_list.append(
                             interface.draw_message(username, text_box.get_edit_text())
                             )
